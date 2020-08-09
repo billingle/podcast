8ed071ab913399912ce03f255a7952ca2406c511
Bill Ingle <bill@inge.com>
2020-08-08 18:02:48 -0700

    created new def show_header

--- program.py
+++ program.py
@@ -2,14 +2,18 @@ import service
 
 
 def main():
-    print("Welcome to the talk python info downloader.")
-    print()
+    show_header()
 
     max_episodes = service.download_info()
 
     for show_id in range(1, max_episodes + 1):
         info = service.get_episode(show_id)
         print("{}. {}".format(info.show_id, info.title))
+
+
+def show_header():
+    print("Welcome to the talk python info downloader.")
+    print()
 
 
 if __name__ == '__main__':
